ab99be6e7e26b6acd04b8481598ab6cbb3dd2a2e
mubarak96-max <mubarakmmm5@gmail.com>
2021-08-05 12:02:38 +0300

    more additions

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,63 +1,45 @@
-import React, { useState } from "react"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { FaCircle, FaImages } from "react-icons/fa"
-import carsData from "../data/cars"
+import "./css/car.css"
 
-const Car = () => {
-  const [cars, setCars] = useState(carsData)
-  console.log(cars)
-
+const Car = ({
+  id,
+  name,
+  price,
+  image,
+  milage,
+  fuel,
+  transmission,
+  featured,
+  images,
+}) => {
+  const pic = getImage(image)
   return (
-    <div className="single-car">
-      {cars.map(car => {
-        const {
-          id,
-          category,
-          condition,
-          make,
-          milage,
-          transmission,
-          fuel,
-          model,
-          name,
-          price,
-          featured,
-        } = car
-        return (
-          <article id={id} className="car">
-            {featured && <p className="isFeatured">featured</p>}
-            <StaticImage
-              src="../images/land cruiser.jpg"
-              width={400}
-              height={250}
-              quality={95}
-              formats={["AUTO", "WEBP", "AVIF"]}
-              alt="vehica"
-            />
-            <span className="images">
-              <FaImages />3
-            </span>
-            <p>{name}</p>
-            <h4>Ushs{price}</h4>
-            <div className="about-car">
-              <span className="milage">
-                <FaCircle size={20} />
-                {milage}kms
-              </span>
-              <h6>
-                <FaCircle size={20} />
-                {transmission}
-              </h6>
-              <h6>
-                <FaCircle size={20} />
-                {fuel}
-              </h6>
-            </div>
-          </article>
-        )
-      })}
-    </div>
+    <article id={id} className="car">
+      {featured && <p className="isFeatured">featured</p>}
+      <GatsbyImage image={pic} />
+      <span className="images">
+        <FaImages />
+        {images.length}
+      </span>
+      <p>{name}</p>
+      <h4>Ushs{price}</h4>
+      <div className="about-car">
+        <span className="milage">
+          <FaCircle size={20} />
+          {milage}kms
+        </span>
+        <h6>
+          <FaCircle size={20} />
+          {transmission}
+        </h6>
+        <h6>
+          <FaCircle size={20} />
+          {fuel}
+        </h6>
+      </div>
+    </article>
   )
 }
-
 export default Car
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,112 +1,133 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Car from "../components/Car"
 import "./css/index.css"
 import Select from "react-select"
 import { FaSearch, FaCar } from "react-icons/fa"
-
-// const makes = [
-//   {
-//     make: "BMW",
-//     models: ["320i", "BMW X3", "X3"],
-//   },
-//   {
-//     make: "Isuzu",
-//     models: ["Isuzu Elf", "Isuzu Aska"],
-//   },
-//   {
-//     make: "Jeep",
-//     models: ["Jeep Grand Cheroke", "2015 Cheroke Model"],
-//   },
-// ]
 
 const make = [
   { value: "BMW", label: "BMW" },
   { value: "Jeep", label: "Jeep" },
 ]
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <div className="intro-hero">
-      <StaticImage
-        style={{ gridArea: "1/1" }}
-        layout="fullWidth"
-        src="../images/background.jpg"
-        width={100}
-        quality={95}
-        formats={["AUTO", "WEBP", "AVIF"]}
-        alt="vehica"
-      />
-      <div className="intro-hero-content">
-        <h2>
-          Find Your <span>Dream </span>Car
-        </h2>
+const IndexPage = ({ data }) => {
+  console.log(data)
+  const {
+    allCars: { cars },
+  } = data
+  console.log(cars)
 
-        <div className="condition-links">
-          <Link to="/">All</Link>
-          <Link to="/">japan used</Link>
-          <Link to="/">New</Link>
-          <Link to="/">uganda used</Link>
-        </div>
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div className="intro-hero">
+        <StaticImage
+          style={{ gridArea: "1/1" }}
+          layout="fullWidth"
+          src="../images/background.jpg"
+          width={100}
+          quality={95}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="vehica"
+        />
+        <div className="intro-hero-content">
+          <h2>
+            Find Your <span>Dream </span>Car
+          </h2>
 
-        <Select options={make} className="select" placeholder="All Makes" />
+          <div className="condition-links">
+            <Link to="/">All</Link>
+            <Link to="/">japan used</Link>
+            <Link to="/">New</Link>
+            <Link to="/">uganda used</Link>
+          </div>
 
-        <Select options={make} className="select" placeholder="All Makes" />
+          <Select options={make} className="select" placeholder="All Makes" />
 
-        <button className="select-button">
-          Search
-          <FaSearch />
-        </button>
-        <div className="vehicle-categories">
-          <div className="vehicle-category">
-            <Link to="/">
-              <FaCar className="vehicle-icon" />
-              <h6>sedan</h6>
-            </Link>
-          </div>
-          <div className="vehicle-category">
-            <Link to="/">
-              <FaCar className="vehicle-icon" />
-              <h6>tractor</h6>
-            </Link>
-          </div>
-          <div className="vehicle-category">
-            <Link to="/">
-              <FaCar className="vehicle-icon" />
-              <h6>coupe</h6>
-            </Link>
-          </div>
-          <div className="vehicle-category">
-            <Link to="/">
-              <FaCar className="vehicle-icon" />
-              <h6>sports utility</h6>
-            </Link>
-          </div>
-          <div className="vehicle-category">
-            <Link to="/">
-              <FaCar className="vehicle-icon" />
-              <h6>hatchback</h6>
-            </Link>
-          </div>
-          <div className="vehicle-category">
-            <Link to="/">
-              <FaCar className="vehicle-icon" />
-              <h6>convertible</h6>
-            </Link>
-          </div>
-          <div className="vehicle-category">
-            <Link to="/">
-              <FaCar className="vehicle-icon" />
-              <h6>truck</h6>
-            </Link>
+          <Select options={make} className="select" placeholder="All Makes" />
+
+          <button className="select-button">
+            Search
+            <FaSearch />
+          </button>
+          <div className="vehicle-categories">
+            <div className="vehicle-category">
+              <Link to="/">
+                <FaCar className="vehicle-icon" />
+                <h6>sedan</h6>
+              </Link>
+            </div>
+            <div className="vehicle-category">
+              <Link to="/">
+                <FaCar className="vehicle-icon" />
+                <h6>tractor</h6>
+              </Link>
+            </div>
+            <div className="vehicle-category">
+              <Link to="/">
+                <FaCar className="vehicle-icon" />
+                <h6>coupe</h6>
+              </Link>
+            </div>
+            <div className="vehicle-category">
+              <Link to="/">
+                <FaCar className="vehicle-icon" />
+                <h6>sports utility</h6>
+              </Link>
+            </div>
+            <div className="vehicle-category">
+              <Link to="/">
+                <FaCar className="vehicle-icon" />
+                <h6>hatchback</h6>
+              </Link>
+            </div>
+            <div className="vehicle-category">
+              <Link to="/">
+                <FaCar className="vehicle-icon" />
+                <h6>convertible</h6>
+              </Link>
+            </div>
+            <div className="vehicle-category">
+              <Link to="/">
+                <FaCar className="vehicle-icon" />
+                <h6>truck</h6>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </Layout>
-)
+
+      {cars.map(car => {
+        console.log(car.image)
+
+        return <Car {...car} />
+      })}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allCars: allContentfulVehica {
+      cars: nodes {
+        fuel
+        id
+        image {
+          gatsbyImageData(quality: 95, placeholder: BLURRED, formats: [AUTO])
+        }
+        milage
+        images {
+          title
+        }
+        name
+        price
+        transmission
+      }
+    }
+  }
+`
 
 export default IndexPage
