0a1b545a7eaa1c3549756fdd93b041ede12d2cf7
mubarak96-max <mubarakmmm5@gmail.com>
2021-08-08 08:32:54 +0300

    more additions

--- src/components/relatedCars.js
+++ src/components/relatedCars.js
@@ -2,40 +2,40 @@ import React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Car from "./Car"
 
-const query = graphql`
-  {
-    all: allContentfulVehica {
-      carsLists: nodes {
-        fuel
-        id
-        image {
-          gatsbyImageData(quality: 95, placeholder: BLURRED, formats: [AUTO])
-        }
-        milage
-        images {
-          title
-        }
-        name
-        category
-        price
-        transmission
-      }
-    }
-  }
-`
+// const query = graphql`
+//   {
+//     all: allContentfulVehica {
+//       carsLists: nodes {
+//         fuel
+//         id
+//         image {
+//           gatsbyImageData(quality: 95, placeholder: BLURRED, formats: [AUTO])
+//         }
+//         milage
+//         images {
+//           title
+//         }
+//         name
+//         category
+//         price
+//         transmission
+//       }
+//     }
+//   }
+// `
 
 const RelatedCars = () => {
-  const data = useStaticQuery(query)
-  const [find, setFind] = useState([])
-  // const {
-  //   all: {
-  //     carsList: { category },
-  //   },
-  // } = data
+  // const data = useStaticQuery(query)
+  // const [find, setFind] = useState([])
+  // // const {
+  // //   all: {
+  // //     carsList: { category },
+  // //   },
+  // // } = data
 
-  const categoryCars = data.allCars.find(car => car.category == "car")
-  console.log(categoryCars)
-  console.log(data)
+  // const categoryCars = data.allCars.find(car => car.category == "car")
+  // console.log(categoryCars)
+  // console.log(data)
 
   return <h2>related cars...</h2>
 }
